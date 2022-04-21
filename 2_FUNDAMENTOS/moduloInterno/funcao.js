2ab1f33e595e40c9ecd294c75ed80383e30b9ed6
joaopfsiqueira <joaopfsiqueiraa@gmail.com>
2022-04-21 18:15:23 -0300

    first commit: fundamentos modulo interno

---
+++ 2_FUNDAMENTOS/moduloInterno/funcao.js
@@ -0,0 +1,7 @@
+//exportando internamente o modulo que exporta uma função.
+
+module.exports = {
+    soma(a,b) {
+        console.log(a + b)
+    }
+}
---
+++ 2_FUNDAMENTOS/moduloInterno/index.js
@@ -0,0 +1,10 @@
+const funcao = require('./funcao');
+
+//acessando a função soma dentro do arquivo ./funcao.js
+const soma = funcao.soma;
+
+
+//como a função espera receber 2 valores, fica da seguinte forma:
+soma(2,3);
+soma(4,10);
+
