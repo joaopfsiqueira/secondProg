22c49d97c0130d9a5b23e62dd7ba4e888e76bfad
joaopfsiqueira <joaopfsiqueiraa@gmail.com>
2022-04-26 06:20:08 -0300

    utilizando console.count e interpolar string.

---
+++ 2_FUNDAMENTOS/explorandoConsole/index.js
@@ -0,0 +1,17 @@
+const x = 10;
+const y = "João"
+const z = [1, 2]
+
+console.log(x, y, z)
+
+//contagem de impressões
+console.count(`O valor de x é: ${x}, contagem`)
+console.count(`O valor de x é: ${x}, contagem`)
+console.count(`O valor de x é: ${x}, contagem`)
+console.count(`O valor de x é: ${x}, contagem`)
+console.count(`O valor de x é: ${x}, contagem`)
+
+
+//variavel entre string 
+//posso chamar qualquer tipo de valor, int, boolean, ou string mesmo. ele vai ser convertido para string de qualquer forma.
+console.log("O nome é %s, ele é programador", y)
