0789601d20e980f0cfe4dc8ec7468577271c2e22
joaopfsiqueira <joaopfsiqueiraa@gmail.com>
2022-04-26 04:30:32 -0300

    feat: ler argumento do node

---
+++ 2_FUNDAMENTOS/ler_argumentos/index.js
@@ -0,0 +1,17 @@
+// pegando nome que está sendo passado pelo comando node, exemplo: node .\index.js nome=joao idade=21
+console.log(process.argv);
+
+const args = process.argv.slice(2)
+
+console.log(args);
+
+const nome = args[0].split("=")[1]
+
+console.log(nome)
+
+const idade = args[1].split("=")[1]
+
+console.log(idade)
+
+
+console.log(`O nome dele é ${nome} e ele tem ${idade} anos`);
