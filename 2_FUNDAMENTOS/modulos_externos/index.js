f4757e8dfd3789fca7ac19bc7c69132f12696f21
joaopfsiqueira <joaopfsiqueiraa@gmail.com>
2022-04-26 04:45:00 -0300

    feat: pegando profissao

--- 2_FUNDAMENTOS/modulos_externos/index.js
+++ 2_FUNDAMENTOS/modulos_externos/index.js
@@ -2,14 +2,15 @@ const minimist = require('minimist');
 //o minimist basicamente deixa melhor visivel os args dentro de um terminal.
 //não só isso como também deixa mais fácil de pegar o valor dentro do terminal. a diferença é que não precisa fazer um split (COMO EM "LER_ARGUMENTOS")
 
-//comando: node index.js --nome=siqueira (diferença no comando: precisa do --antes do nome)
+//comando: node index.js --nome=siqueira --profissao=developer(diferença no comando: precisa do --antes do nome)
 
 
-//pegando argumentos
+//pegando argumentos à partir do segundo elemento para frente, ou certo, depois do index.js e colocando dentro de um array
 const args = minimist(process.argv.slice(2));
 
 console.log(args);
 
 const nome = args['nome']
+const profissao = args['profissao']
 
-console.log(nome)
+console.log(nome, profissao)
